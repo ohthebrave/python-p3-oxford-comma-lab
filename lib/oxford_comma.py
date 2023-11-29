fc0e6ed46f337cf5a3e35ab8468662b93074fc46
George <georgejoj28@gmail.com>
2023-11-29 13:12:56 +0300

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ''
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        last_item = items[-1]
+        preceding_items = ', '.join(items[:-1])
+        return f'{preceding_items}, and {last_item}'
+    
+
+
+comma = ["fiddleheads", "okra", "kohlrabi"]
+
+print(len(comma))
